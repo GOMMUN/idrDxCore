87b61a006ec271533a193a5be1cba659e15d12bf
sangmoon <sangmoon@HANU>
2023-07-25 16:27:31 +0900

    수정

--- src/main/java/com/idr/pdd/common/BlockKitDataParshing.java
+++ src/main/java/com/idr/pdd/common/BlockKitDataParshing.java
@@ -8,100 +8,110 @@ import com.google.gson.JsonParser;
 
 public class BlockKitDataParshing {
 	
-	public String setUrl(String level) {
+	public static String underProduction(String blockKit, String btnString, String btnUrl, String plantName, String materialName, int planQty, int prodQty, int percent) {
 		
-		return null;
+		JsonObject jsonObject = (JsonObject)new JsonParser().parse(blockKit);
+	
+		JsonArray jsonArray = jsonObject.get("blocks").getAsJsonArray();
+		
+		int index = 0;
+		for (JsonElement jsonElement : jsonArray) {
+			
+			if(index == 2) {
+				// 회사명(공장명)
+				jsonElement.getAsJsonObject().get("inlines").getAsJsonArray().get(1)
+							.getAsJsonObject().addProperty("text", plantName);
+			}else if(index == 3) {
+				// 자재명
+				jsonElement.getAsJsonObject().get("inlines").getAsJsonArray().get(1)
+							.getAsJsonObject().addProperty("text", materialName);
+			}else if(index == 4) {
+				// 생산/계획 
+				String str = prodQty + "/" + planQty + ", " + percent + "%";
+				jsonElement.getAsJsonObject().get("inlines").getAsJsonArray().get(1)
+							.getAsJsonObject().addProperty("text", str);
+			}else if(index == 6) {
+				// 버튼 text
+				jsonElement.getAsJsonObject().addProperty("text", btnString);
+
+				// 버튼 URL
+				jsonElement.getAsJsonObject().get("action").getAsJsonObject().addProperty("value", btnUrl);
+			}
+			
+			
+			index++;
+		}
+		blockKit = jsonObject.toString();
+		
+		return blockKit;
 	}
 
-	public static String underProduction(String level, String blockKit, String plantName, int planQty, int prodQty, int percent) {
+	public static String notoperatePress(String blockKit, String btnString, String btnUrl, String plantName, String lineName) {
 		
-		if("OCCUR".equals(level)) {
-			// 발생
-			//JSONParser parser = new JSONParser(blockKit);
+		JsonObject jsonObject = (JsonObject)new JsonParser().parse(blockKit);
+	
+		JsonArray jsonArray = jsonObject.get("blocks").getAsJsonArray();
+		
+		int index = 0;
+		for (JsonElement jsonElement : jsonArray) {
 			
-			JsonObject jsonObject = (JsonObject)new JsonParser().parse(blockKit);
-			
-			JsonArray jsonArray = jsonObject.get("blocks").getAsJsonArray();
-			
-			System.out.println(jsonObject);
-			
-			int index = 0;
-			for (JsonElement jsonElement : jsonArray) {
-				
-				if(index == 2) {
-					// 회사명(공장명)
-					jsonElement.getAsJsonObject().get("inlines").getAsJsonArray().get(1)
-								.getAsJsonObject().addProperty("text", plantName);
-				}else if(index == 3) {
-					// 자재명
-					jsonElement.getAsJsonObject().get("inlines").getAsJsonArray().get(1)
-								.getAsJsonObject().addProperty("text", Integer.toString(planQty));
-				}else if(index == 4) {
-					// 생산/계획 
-					String str = prodQty + "/" + planQty + ", " + percent + "%";
-					jsonElement.getAsJsonObject().get("inlines").getAsJsonArray().get(1)
-								.getAsJsonObject().addProperty("text", str);
-				}else if(index == 5) {
-					
-					// 버튼 text
-					jsonElement.getAsJsonObject().addProperty("text", "통보");
+			if(index == 2) {
+				// 회사명(공장명)
+				jsonElement.getAsJsonObject().get("inlines").getAsJsonArray().get(1)
+							.getAsJsonObject().addProperty("text", plantName);
+			}else if(index == 3) {
+				// 자재명
+				jsonElement.getAsJsonObject().get("inlines").getAsJsonArray().get(1)
+							.getAsJsonObject().addProperty("text", lineName);
+			}else if(index == 5) {
+				// 버튼 text
+				jsonElement.getAsJsonObject().addProperty("text", btnString);
 
-					// 버튼 URL
-					jsonElement.getAsJsonObject().get("action").getAsJsonObject().addProperty("value", "123");
-				}
-				
-				index++;
+				// 버튼 URL
+				jsonElement.getAsJsonObject().get("action").getAsJsonObject().addProperty("value", btnUrl);
 			}
-			blockKit = jsonObject.toString();
 			
-		}else if("NOTICE".equals(level)) {
-			// 통보
-			//JSONParser parser = new JSONParser(blockKit);
 			
-			JsonObject jsonObject = (JsonObject)new JsonParser().parse(blockKit);
-			
-			JsonArray jsonArray = jsonObject.get("blocks").getAsJsonArray();
-			
-			System.out.println(jsonObject);
-			
-			int index = 0;
-			for (JsonElement jsonElement : jsonArray) {
-				
-				if(index == 5) {
-					
-					// 버튼 text
-					jsonElement.getAsJsonObject().addProperty("text", "확인");
-					
-					// 버튼 URL
-					jsonElement.getAsJsonObject().get("action").getAsJsonObject().addProperty("value", "123");
-				}
-				
-				index++;
-			}
-			blockKit = jsonObject.toString();
-			
-		}else if("CONFIRM".equals(level)) {
-			// 확인
-			//JSONParser parser = new JSONParser(blockKit);
-			
-			JsonObject jsonObject = (JsonObject)new JsonParser().parse(blockKit);
-			
-			JsonArray jsonArray = jsonObject.get("blocks").getAsJsonArray();
-			
-			System.out.println(jsonObject);
-			
-			int index = 0;
-			for (JsonElement jsonElement : jsonArray) {
-				
-				if(index == 5) {
-					// 버튼 URL
-					jsonElement.getAsJsonObject().get("action").getAsJsonObject().addProperty("value", "123");
-				}
-				
-				index++;
-			}
-			blockKit = jsonObject.toString();
+			index++;
 		}
+		blockKit = jsonObject.toString();
+		
+		return blockKit;
+	}
+	
+
+	public static String firstArticleCaution(String blockKit, String btnString, String btnUrl, String plantName, String lineName) {
+		
+		JsonObject jsonObject = (JsonObject)new JsonParser().parse(blockKit);
+	
+		JsonArray jsonArray = jsonObject.get("blocks").getAsJsonArray();
+		
+		int index = 0;
+		for (JsonElement jsonElement : jsonArray) {
+			
+			if(index == 2) {
+				// 회사명(공장명)
+				jsonElement.getAsJsonObject().get("inlines").getAsJsonArray().get(1)
+							.getAsJsonObject().addProperty("text", plantName);
+			}else if(index == 3) {
+				// 자재명
+				jsonElement.getAsJsonObject().get("inlines").getAsJsonArray().get(1)
+							.getAsJsonObject().addProperty("text", lineName);
+			}else if(index == 4) {
+				// 자재명
+				jsonElement.getAsJsonObject().get("inlines").getAsJsonArray().get(1)
+							.getAsJsonObject().addProperty("text", lineName);
+			}else if(index == 6) {
+				// 버튼 text
+				jsonElement.getAsJsonObject().addProperty("text", btnString);
+
+				// 버튼 URL
+				jsonElement.getAsJsonObject().get("action").getAsJsonObject().addProperty("value", btnUrl);
+			}
+			
+			index++;
+		}
+		blockKit = jsonObject.toString();
 		
 		return blockKit;
 	}
